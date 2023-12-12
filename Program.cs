0d49f7c39957db59c5c8a1865e619a7abdbd2c1b
ScOrP <alex_77_90@mail.ru>
2023-12-12 15:29:22 +0500

    Добавлен метод поиска длины нового массива

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = {"Hello", "2", "world", ":-)"};
+
+int FindLen(string[] arr)
+{
+    int res = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            res++;
+        }
+    }
+    return res;
+}
